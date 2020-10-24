5f2d8e7d9335c737bd9dcf79959f7560ddd2ed6d
erislandio.soares <erislandio.soares@acct.global>
2020-10-24 16:23:48 -0300

    added controller / server / test functional with mocks data

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import "./utils/moduleAlias";
+import { Server } from "@overnightjs/core";
+import bodyParser from "body-parser";
+import { ForecastController } from "./controllers/forecast";
+import { Application } from "express";
+
+export class SetupServer extends Server {
+  constructor(private port = 3000) {
+    super(true);
+  }
+
+  public init(): void {
+    this.setupeExpress();
+    this.setupeController();
+  }
+
+  private setupeExpress(): void {
+    this.app.use(bodyParser.json());
+  }
+
+  private setupeController(): void {
+    const forecast = new ForecastController();
+    this.addControllers([forecast]);
+  }
+  public getApp(): Application {
+    return this.app;
+  }
+}
